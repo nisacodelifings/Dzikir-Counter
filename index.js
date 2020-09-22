a1d21086476f769e71dfbc3fccb66f1f82240d4c
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-22 14:49:21 +0700

    sw.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./sw.js";
+import "./manifest.json"
+
+ // ServiceWorker Registration
+ if('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          
+      }, function(err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
